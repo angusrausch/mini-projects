3e7dc5b60f55eecd12254768d35fa91e45af159a
Angus Rausch <53633086+angusrausch@users.noreply.github.com>
2025-12-06 12:00:37 +1100

    Added spash screen to cli

--- ns_spam_java/src/main/java/ns/CliRequest.java
+++ ns_spam_java/src/main/java/ns/CliRequest.java
@@ -10,6 +10,16 @@ package ns;
  */
 public class CliRequest extends Request {
 
+    public static final String RED     = "\033[31m";
+    public static final String GREEN   = "\033[32m";
+    public static final String YELLOW  = "\033[33m";
+    public static final String BLUE    = "\033[34m";
+    public static final String MAGENTA = "\033[35m";
+    public static final String CYAN    = "\033[36m";
+    public static final String RESET   = "\033[0m";
+    public static final String BOLD    = "\033[1m";
+    public static final String ORANGE  = "\033[38;5;208m";
+
     public CliRequest(String[] args) {
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
@@ -82,8 +92,78 @@ public class CliRequest extends Request {
                     this.endless = true;
                     break;
                 default:
-
                     throw new AssertionError();
+            }
+        }
+        if (nameserver == null) {
+            System.out.println(RED + "No Nameserver selected. You must select a nameserver." + RESET);
+            System.exit(1);
+        }
+
+        print_options();
+    }
+
+    private void print_options() {
+        String asciiBanner = String.format(
+            """
+            %s%s
+                        _   _  _____       _____ _____        __  __ 
+                        | \\ | |/ ____|     / ____|  __ \\ /\\   |  \\/  |
+                        |  \\| | (___ _____| (___ | |__) /  \\  | \\  / |
+                        | . ` |\\___ \\______\\___ \\|  ___/ /\\ \\ | |\\/| |
+                        | |\\  |____) |     ____) | |  / ____ \\| |  | |
+                        |_| \\_|_____/     |_____/|_| /_/    \\_\\_|  |_|                                   
+            %s
+            """,
+            CYAN, BOLD, RESET
+        );
+
+        // Build message
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("%sThank you for using %sNS-Spam%s.\n",
+                YELLOW, MAGENTA, RESET));
+
+        sb.append(String.format(
+                "%sThis application should only be run on nameservers you have permission from the owner to use.%s\n",
+                YELLOW, RESET));
+
+        sb.append(String.format(
+                "%sYou have selected nameserver %s%s%s %s to run on.%s\n",
+                YELLOW, BOLD, BLUE, nameserver, RESET, YELLOW, RESET));
+
+        if (random) {
+            sb.append(String.format(
+                    "%sRandom option selected.%s %sThis will create random subdomains off of your domain %s%s%s %s with a length of 3-7 characters.%s\n",
+                    YELLOW, RESET, YELLOW, BOLD, BLUE, domain, RESET, YELLOW, RESET
+            ));
+        } else {
+            sb.append(String.format(
+                    "%sYou have selected the domain %s%s%s %s to run on.%s\n",
+                    YELLOW, BOLD, BLUE, domain, RESET, YELLOW, RESET
+            ));
+        }
+
+        System.out.println(asciiBanner);
+        System.out.println(MAGENTA + "-".repeat(114) + RESET);
+        System.out.println(sb);
+        System.out.println(MAGENTA + "-".repeat(114) + RESET);
+        System.out.println("\n" + YELLOW + "Press enter to begin or CTRL+C to exit" + RESET);
+
+        Thread shutdownHook = new Thread(() -> {
+            System.out.println("\n" + GREEN + "Exiting gracefully (Ctrl+C detected)" + RESET);
+        });
+        Runtime.getRuntime().addShutdownHook(shutdownHook);
+
+        try {
+            System.in.read();
+        } catch (Exception e) {
+            System.out.println("\n" + GREEN + "Exiting gracefully" + RESET);
+            System.exit(0);
+        } finally {
+            try {
+                Runtime.getRuntime().removeShutdownHook(shutdownHook);
+            } catch (IllegalStateException ignored) {
             }
         }
     }
